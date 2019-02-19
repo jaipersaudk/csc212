9a38a261211cd5d7e2c01df92380e2f0b2cb6a1d
Kevin Jaipersaud (csc103 student) <kjaiper000@citymail.cuny.edu>
2019-02-18 22:20:42 -0500

    Read Chapter 4.1

---
+++ chapter4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+/* Self-Test Exercise #3  */
+/*
+int main()
+{
+  int *p1;
+  p1 = new int[1000];
+  for (size_t i = 0; i < 1000; ++i)
+  {
+    p1[i] = i;
+  }
+
+  delete [] p1;
+
+  return 0;
+}
+*/
