e90e0fac1bcdf339f4562b95012e76decec1f425
kevinJ <jaipersaudk@gmail.com>
2019-03-07 22:32:51 -0500

    Fail Tests 1, 2, 3. Pass test 4, 5

--- project3/sequence2.cpp
+++ project3/sequence2.cpp
@@ -12,39 +12,52 @@ namespace main_savitch_4
     data = new value_type[initial_capacity];
     capacity = initial_capacity;
     used = 0;
-    //current_index = 0;
+    current_index = 0;
+    if (initial_capacity < 1)
+      capacity = 1;
   }
 
   sequence::sequence(const sequence& source)
   {
-    data = new value_type[source.capacity];
-    capacity = source.capacity;
     used = source.used;
-    //copy(source.data, source.data + used, data);
+    current_index = source.current_index;
+    capacity = source.capacity;
+    data = new value_type[source.capacity];
+    for (size_type i = 0; i < used; ++i)
+    {
+      data[i] = source.data[i];
+    }
   }
 
   sequence::~sequence()
   {
     delete [] data;
+    data = NULL;
   }
 
   // MODIFICATION MEMBER FUNCTIONS
 
   void sequence::resize(size_type new_capacity)
   {
-      value_type* big_array; //create a new array
+      value_type* temp_arr; //create a new array
 
       if ((new_capacity = capacity))
         return; //check if the new allocated memory is the same size as the old one
 
       if (new_capacity < used)
-        new_capacity = used;
+        capacity = used;
 
-      big_array = new value_type[new_capacity];
-      // copy(data, data + used, big_array);
+      if (new_capacity < 1)
+        new_capacity = 1;
+
+      temp_arr = new value_type[capacity];
+      for (size_type i = 0; i < used; ++i)
+      {
+        temp_arr[i] = data[i];
+      }
+
       delete [] data;
-      data = big_array;
-      capacity = new_capacity;
+      data = temp_arr;
   }
 
   void sequence::start()
@@ -55,55 +68,63 @@ namespace main_savitch_4
   void sequence::advance()
   {
     assert(is_item());
-    ++current_index;
+    current_index = current_index + 1;
   }
 
   void sequence::insert(const value_type& entry)
   {
     // check if the array is full
     if (used == capacity)
-      resize((capacity*2)+1);
+      resize(size_type(capacity*2)+1);
 
     if(!is_item())
-      current_index = 0;
-
-    for (size_type i = used; i > current_index; --i)
     {
-      data[i] = data[i-1];
+      current_index = 0;
+      for (size_type i = used + 1; i > current_index; --i)
+      {
+        data[i] = data[i-1];
+      }
+      data[current_index] = entry;
+      ++used;
     }
-    data[current_index] = entry;
-    ++used;
-    // data[current_index] = entry;
-    // ++current_index;
-    // ++used;
+
+    else
+    {
+      for (size_type i = used + 1; i > current_index; --i)
+      {
+        data[i] = data[i-1];
+      }
+      data[current_index] = entry;
+      ++used;
+    }
   }
 
   void sequence::attach(const value_type& entry)
   {
     if(used == capacity)
-      resize((capacity*2)+1);
+      resize(size_type(capacity*2)+1);
 
     if (!is_item())
-      current_index = used-1;
-
-    for (size_type i = used; i > current_index + 1; --i)
     {
-      data[i] = data[i-1];
+      data[current_index] = entry;
+      ++used;
     }
-    data[current_index+1] = entry;
-    ++current_index;
-    ++used;
-
-    // data[current_index+1] = entry;
-    // ++current_index;
-    // ++used;
-
+    else
+    {
+      ++current_index;
+      for (size_type i = used + 1; i > current_index; --i)
+      {
+        data[i] = data[i-1];
+      }
+      data[current_index] = entry;
+      ++used;
+    }
   }
 
   void sequence::remove_current( )
   {
     assert(is_item());
-    for (size_t i = current_index; i < used; ++i)
+    for (size_t i = current_index; i < used - 1; ++i)
     {
       data[i] = data[i+1];
     }
@@ -125,7 +146,6 @@ namespace main_savitch_4
     }
 
     used = source.used;
-    // copy(source.data, source.data + used, data);
   }
 
 
@@ -142,6 +162,7 @@ namespace main_savitch_4
 
   sequence::value_type sequence::current() const
   {
+    assert(is_item());
     return current_index;
   }
 
