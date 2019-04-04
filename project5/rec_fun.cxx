36409d25fd28af302385b972a9a30863c5b6ec4e
kevinJ <jaipersaudk@gmail.com>
2019-04-04 19:26:12 -0400

    Started Bears and Triangle

--- project5/rec_fun.cxx
+++ project5/rec_fun.cxx
@@ -1,2 +1,99 @@
-#include "rec_fun.h"
+// #include "rec_fun.h"
 #include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class recursion
+{
+  public:
+    void triangle(ostream& outs, unsigned int m, unsigned int n)
+    {
+      // base case
+      if (m >= n)
+        return;
+
+      else
+      {
+        for (int i = m; i <= n; i++)
+        {
+          outs << "*";
+        }
+        outs << "\n";
+
+        triangle(cout, m-1, n);
+        for (int i = m; i <= n; i++)
+        {
+          outs << "*";
+        }
+        outs << "\n";
+      }
+
+    }
+
+    void numbers(ostream& outs, const string& prefix, unsigned int levels)
+    {
+
+    }
+
+    bool bears(unsigned int n)
+    {
+      // base case
+      if (n == 42)
+        return true;
+
+      if (n < 42)
+        return false;
+
+      if (n != 42)
+      {
+
+        if ((n%5)==0)
+        {
+          n = n-42;
+          return (bears(n));
+        }
+
+        if ((n%2)==0)
+        {
+          n = n - (n/2);
+          return (bears(n));
+        }
+
+        if ( (n%3)==0 || ((n%4)==0) )
+        {
+          unsigned int i = n%10;
+          unsigned int j = ((n%100)/10);
+          n = n-(i*j);
+          return (bears(n));
+        }
+
+      }
+
+      return false;
+    }
+
+    void pattern(ostream& outs, unsigned int n, unsigned int i)
+    {
+
+    }
+
+};
+
+int main ()
+{
+  // recursion rec1;
+  // rec1.triangle(cout, 3,5);
+
+  // recursion rec2;
+  // rec2.numbers(cout, THERBLIG, 2);
+
+  recursion rec3;
+  if (rec3.bears(250))
+    cout << "You Won!" << endl;
+
+  else
+    cout << "You Lost! :(" << endl;
+
+  return 0;
+}
--- project5/rec_fun.h
+++ project5/rec_fun.h
@@ -1,4 +1,49 @@
-#ifndef
-#define
+#ifndef RECURSIVE_FUCNTIONS_H
+#define RECURSIVE_FUCNTIONS_H
 
+// any includes
+
+// any namespace
+
+class rec_fun
+{
+  public:
+    void triangle (ostream& outs, unsigned int m, unsigned int n);
+    // Precondition: m <= n
+    // Postcondition: The function has printed a pattern of 2*(n-m+1) lines
+    // to the output stream outs. The first line contains m asterisks, the next
+    // line contains m+1 asterisks, and so on up to a line with n asterisks.
+    // Then the pattern is repeated backwards, going n back down to m.
+
+    /* Example output:
+    triangle(cout, 3, 5) will print this to cout:
+    ***
+    ****
+    *****
+    *****
+    ****
+    ***
+    */
+
+    void numbers(ostream& outs, const String& prefix, unsigned int levels);
+
+    bool bears(int n);
+    // Postcondition: A true return value means that it is possible to win
+    // the bear game by starting with n bears. A false return value means that
+    // it is not possible to win the bear game by starting with n bears.
+    // Examples:
+    // bear(250) is true (as shown above)
+    // bear(42) is true
+    // bear(84) is true
+    // bear(53) is false
+    // bear(41) is false
+
+    void pattern(ostream& outs, unsigned int n, unsigned int i);
+    // Precondition: n is a power of 2 greater than zero.
+    // Postcondition: A pattern based on the above example has been
+    // printed to the ostream outs. The longest line of the pattern has
+    // n stars beginning in column i of the output.
+    //For example, The above pattern is produced by the call pattern(cout, 8, 0).
+
+}
 #endif
