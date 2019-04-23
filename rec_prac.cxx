2a337252eeee09686cd72c5818143d013a30c31c
kevinJ <jaipersaudk@gmail.com>
2019-04-23 14:51:10 -0400

    Finished gcb

--- rec_prac.cxx
+++ rec_prac.cxx
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<cmath>
+#include<iomanip>
 using namespace std;
 
 class recursion
@@ -7,22 +8,35 @@ class recursion
   public:
     int digitDifference(int a, int b)
     {
+      // base case
       if ((a < 10) && (b < 10))
-      {
-        cout << abs(a-b) << endl;
-      }
+        cout << abs(a-b) << std::flush; //std::flush keeps the output on the same line
 
-      digitDifference(a/10,b/10);
-      cout << abs(a/10-b/10) << endl;
+      if ((a >=10) && (b>=10))
+      {
+        digitDifference(a/10,b/10);
+        cout << abs((a%10)-(b%10)) << std::flush; //std::flush keeps the output on the same line
+      }
     }
 
     int lucky7(int n)
     {
-      if (n < 10 && n!=7)
+
+    }
+
+    int gcb(int n, int m)
+    {
+      if (n == m)
         return n;
 
-      else
-        return n*11;
+      if (n != m)
+      {
+        if (n > m)
+          gcb(n/10,m);
+
+        else
+          gcb(n,m/10);
+      }
     }
 
     int removeFirst(int n)
@@ -33,18 +47,44 @@ class recursion
       return removeFirst(n/10) * 10 + n%10;
     }
 
+    int secondDown(int n)
+    {
+      if (n < 100 && n%10 == 0)
+        cout << n << endl;
+
+      if (n < 100 && n%10 !=0)
+        cout << n-1 << endl;
+
+      else
+      {
+        secondDown(n/10);
+      }
+    }
+
 };
 
 int main()
 {
-  recursion rec1;
-  cout << rec1.digitDifference(43,27) << endl;
+  // Testing digitDifference
+  // recursion rec1;
+  // rec1.digitDifference(4519,1187);
 
+  // Testing lucky7
   // recursion rec2;
-  // cout << rec2.lucky7(747) << endl;
-  //
+  // cout << rec2.lucky7(7) << endl;
+
+
+  // Testing gcb
+  // recursion rec3;
+  // cout << rec3.gcb(136578,1357) << endl;
+
+  // Tesing removeFirst
   // recursion rec4;
   // cout << rec4.removeFirst(56723) << endl;
+
+  // Testing secondDown
+  recursion rec5;
+  rec5.secondDown(99);
 
   return 0;
 }
